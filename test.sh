25c47a638191f8602a2b6089f654a7f25eabab23
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-01 12:19:37 +0100

    Another try

--- test.sh
+++ test.sh
@@ -1,17 +1,9 @@
-OUT="
-## <0001f9d0> Your preview environment is ready! 🎉
-
-### [https://graph-brew-a4ynmiu7g-productbrew.vercel.app](https://graph-brew-a4ynmiu7g-productbrew.vercel.app)
-
-< __VERCEL_DEPLOYMENT -->
-<!-- __VERCEL_PREVIEW_URL: asd -->"
-
   REGEX="(__VERCEL_PREVIEW_URL: )(.*)( -->)"
   COMMENT_BODY=""
   
   [[ $COMMENT_BODY =~ $REGEX ]]
   
-  if [[ ${BASH_REMATCH[2]} ]]; then
+  if [[ $COMMENT_BODY =~ $REGEX ]]; then
       echo "VERCEL_PREVIEW_URL=${BASH_REMATCH[2]}" >> $GITHUB_OUTPUT
   else
       echo "No previous Preview Environment"
