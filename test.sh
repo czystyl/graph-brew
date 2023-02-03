8030f86b848ea8d7c787a4930be058de5603ce3b
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-03 21:44:45 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,11 +1,40 @@
-  echo "cG9zdGdyZXM6Ly9wb3N0Z3Jlczpma2pjNUo3RmpESldab0ZANDcxOWM4MGI2ZmU5MTFiMDkxYTdj MDUxMjRiNjRlZWVjZS5mbHkuZGV2OjU0MzIK"
-  echo "cG9zdGdyZXM6Ly9wb3N0Z3Jlczpma2pjNUo3RmpESldab0ZANDcxOWM4MGI2ZmU5MTFiMDkxYTdj MDUxMjRiNjRlZWVjZS5mbHkuZGV2OjU0MzIK"
-  
-  if [[ "" ]]; then
-    echo esa
-  elif [[ echo -e "cG9zdGdyZXM6Ly9wb3N0Z3Jlczpma2pjNUo3RmpESldab0ZANDcxOWM4MGI2ZmU5MTFiMDkxYTdj MDUxMjRiNjRlZWVjZS5mbHkuZGV2OjU0MzIK" ]]; then
-    echo elo        
-  else
-    echo ERROOOORRRRR
+APP_NAME="hello-world-998"
+
+
+flyctl apps destroy $APP_NAME --yes
+
+echo "deleted, creating new"
+
+PG_CREATE_COMMAND="
+    flyctl postgres create \
+        --name hello-world-998 \
+        --org product-brew \
+        --region waw \
+        --vm-size shared-cpu-1x - 256 \
+        --initial-cluster-size 1 \
+        --volume-size 1 \
+        --image-ref flyio/postgres:14
+"
+
+PG_CREATE_OUTPUT=$($PG_CREATE_COMMAND)
+
+echo "created"
+
+echo $PG_CREATE_OUTPUT
+
+REGEX="(Connection string: )(.*)( Save)"
+
+if [[ $(echo $PG_CREATE_OUTPUT) =~ $REGEX ]]; then
+    INTERNAL_CONNECTION_STRING=${BASH_REMATCH[2]}
+else
+    echo "OPS problem"
     exit 1
-  fi
+fi
+
+
+echo $INTERNAL_CONNECTION_STRING
+echo $INTERNAL_CONNECTION_STRING | base64
+echo $INTERNAL_CONNECTION_STRING | sed "s/.internal:/.fly.dev:/g"
+echo $INTERNAL_CONNECTION_STRING | sed "s/.internal:/.fly.dev:/g" | base64
+
+
