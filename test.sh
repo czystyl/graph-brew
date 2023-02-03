2bd83e3a2297ab1acc2110c333cbaf52a1c96312
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-03 14:25:51 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-INTERNAL_CONNECTION_STRING=3213123123
-CONNECTION_STRING=$(echo $INTERNAL_CONNECTION_STRING | sed "s/.internal:/.fly.dev:/g" | base64) 
+INTERNAL_CONNECTION_STRING="postgres://postgres:eUPyjfZ2gw7o1Hu@4719c80b6fe911b091a7c05124b64eeece.fly.dev:5432"
 
+CONNECTION_STRING=$(echo $INTERNAL_CONNECTION_STRING | sed "s/.internal:/.fly.dev:/g" | base64) 
 
 echo $CONNECTION_STRING
