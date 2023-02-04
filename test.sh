dc863534c9a2f261134dcfa948fe85af9b6b352d
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-04 06:45:19 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,40 +1,5 @@
-APP_NAME="hello-world-998"
+TEST_STRING="Hello World!"
 
+TEST=$(echo $TEST_STRING | sed 's/ //g')
 
-flyctl apps destroy $APP_NAME --yes
-
-echo "deleted, creating new"
-
-PG_CREATE_COMMAND="
-    flyctl postgres create \
-        --name hello-world-998 \
-        --org product-brew \
-        --region waw \
-        --vm-size shared-cpu-1x - 256 \
-        --initial-cluster-size 1 \
-        --volume-size 1 \
-        --image-ref flyio/postgres:14
-"
-
-PG_CREATE_OUTPUT=$($PG_CREATE_COMMAND)
-
-echo "created"
-
-echo $PG_CREATE_OUTPUT
-
-REGEX="(Connection string: )(.*)( Save)"
-
-if [[ $(echo $PG_CREATE_OUTPUT) =~ $REGEX ]]; then
-    INTERNAL_CONNECTION_STRING=${BASH_REMATCH[2]}
-else
-    echo "OPS problem"
-    exit 1
-fi
-
-
-echo $INTERNAL_CONNECTION_STRING
-echo $INTERNAL_CONNECTION_STRING | base64
-echo $INTERNAL_CONNECTION_STRING | sed "s/.internal:/.fly.dev:/g"
-echo $INTERNAL_CONNECTION_STRING | sed "s/.internal:/.fly.dev:/g" | base64
-
-
+echo $TEST
