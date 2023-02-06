fb109cdc40527d6720ec7496900d78596af56613
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-06 16:30:52 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,22 +1,16 @@
-  echo "4719c80b6fe911b091a7c05124"
-  echo "cG9zdGdyZXM6Ly9wb3N0Z3Jlczp0T09QaVpIc1Z5RnI1dUFANDcxOWM4MGI2ZmU5MTFiMDkxYTdjMDUxMjQuZmx5LmRldjo1NDMyCg=="                  
-  echo ""
-  
-  NEW_DATABASE_URL=$(echo "cG9zdGdyZXM6Ly9wb3N0Z3Jlczp0T09QaVpIc1Z5RnI1dUFANDcxOWM4MGI2ZmU5MTFiMDkxYTdjMDUxMjQuZmx5LmRldjo1NDMyCg==" | base64 -d)
-  echo $NEW_DATABASE_URL
-  EXISTING_DATABASE_URL=""         
-  
-  echo $NEW_DATABASE_URL
-  echo $EXISTING_DATABASE_URL
-  
-  if [[ $NEW_DATABASE_URL ]]; then
-    DATABASE_URL_KEY="DATABASE_URL_4719c80b6fe911b091a7c05124"
-    
-    # vercel env rm $DATABASE_URL_KEY --yes --token=*** || true
-    # echo $NEW_DATABASE_URL | vercel env add $DATABASE_URL_KEY preview --token=***
-    echo "DATABASE_URL=$NEW_DATABASE_URL" >> $GITHUB_ENV
-  elif [[ $EXISTING_DATABASE_URL ]]; then
-    echo "DATABASE_URL=$NEW_DATABASE_URL" >> $GITHUB_ENV
-  else
-    echo ops && exit 1
-  fi
+ENV_VARS=$(cat .vercel/.env.preview.local)
+DATABASE_URL_KEY=DATABASE_URL_4719c80b6fe911b091a7c05124
+DB_REGEX="$DATABASE_URL_KEY=\"([^\"]*)\""
+
+if [[ $ENV_VARS =~ $DB_REGEX ]]; then
+EXISTING_URL=$(echo ${BASH_REMATCH[1]})
+echo $EXISTING_URL 
+# EXISTING_URL="${EXISTING_URL//$'\n'/''}"
+echo $EXISTING_URL
+EXISTING_URL=$(echo $EXISTING_URL | base64)
+echo $EXISTING_URL
+EXISTING_URL="${EXISTING_URL//$'\n'/''}"
+echo $EXISTING_URL
+
+# echo "URL=$EXISTING_URL" >> $GITHUB_OUTPUT
+fi
