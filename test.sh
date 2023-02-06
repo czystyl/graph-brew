727b7f314de2fffbf4aa1c88e04893e851e2cf33
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-06 14:03:16 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-  content="cG9zdGdyZXM6Ly9wb3N0Z3Jlczp2OUxYeGFoanFYREhzNGFANDcxOWM4MGI2ZmU5MTFiMDkxYTdj
-MDUxMjQuZmx5LmRldjo1NDMyCg=="                  
-  
+VAR="postgres://postgres:eE7F4AsNCeSiRE4@4719c80b6fe911b091a7c05124.fly.dev:5432
 
 
-# remove new line from content using sed
-    content=$(echo $content | sed 's/\ //g')
-echo $content
+a"
+
+# remove new line and space using sed
+VAR=$(echo $VAR | sed 's/ //g' | sed 's/\n//g')
+
+echo $VAR
