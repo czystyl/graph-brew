b1616f9ef79e357a508640141dc8e7c794eb8a6d
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-06 06:41:55 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,20 +1,16 @@
 vars=$(cat .vercel/.env.preview.local)
 
 DATABASE_URL_KEY=DATABASE_URL_123
-
-echo $DATABASE_URL_KEY
-
 DB_REGEX="$DATABASE_URL_KEY=\"([^\"]*)\""
 
-# regexp to match value in quotes only to next quote 
-
-
-echo $DB_REGEX
-
-if [[ $vars =~ $DB_REGEX ]]; then
-    # echo "DATABASE_URL=${BASH_REMATCH[1]}" >> $GITHUB_ENV
+if [[ $vars = $DB_REGEX ]]; then    
     echo ${BASH_REMATCH[1]}
-
 else
     echo "No match"
+fi
+
+STR='GNU/Linux is an operating system'
+SUB='DATABASE_URL_123="elo1"'
+if [[ "$vars" != *"$SUB"* ]]; then
+  echo "It's there."
 fi
