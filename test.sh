256a155880d49080c70d5553600baa922081febe
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-06 10:45:05 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,16 +1,11 @@
 vars=$(cat .vercel/.env.preview.local)
 
 DATABASE_URL_KEY=DATABASE_URL_123
+
 DB_REGEX="$DATABASE_URL_KEY=\"([^\"]*)\""
 
-if [[ $vars = $DB_REGEX ]]; then    
+if [[ $vars =~ $DB_REGEX ]]; then
     echo ${BASH_REMATCH[1]}
 else
     echo "No match"
-fi
-
-STR='GNU/Linux is an operating system'
-SUB='DATABASE_URL_123="elo1"'
-if [[ "$vars" != *"$SUB"* ]]; then
-  echo "It's there."
 fi
