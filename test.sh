d675aa27474a88df6fcc16e21f3d5d702382f2f9
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-06 11:37:30 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,20 +1,24 @@
-vars=$(cat .vercel/.env.preview.local)
+  NEW_DATABASE_URL=""
+  EXISTING_DATABASE_URL=""         
+  
+  echo $NEW_DATABASE_URL
+  echo $EXISTING_DATABASE_URL
+  
+  if [ $NEW_DATABASE_URL ]; then
+    DATABASE_URL_KEY="DATABASE_URL_4719c80b6fe911b091a7c05124"
+    
+    vercel env rm $DATABASE_URL_KEY --yes --token=*** || true
+    echo $NEW_DATABASE_URL | vercel env add $DATABASE_URL_KEY preview --token=***
+    echo "DATABASE_URL=$NEW_DATABASE_URL" >> $GITHUB_ENV
+  elif [ $EXISTING_DATABASE_URL ]; then
+    echo "DATABASE_URL=$NEW_DATABASE_URL" >> $GITHUB_ENV
+  else
+    echo ops && exit 1
+  fi
 
-DATABASE_URL_KEY=DATABASE_URL_123
 
-DB_REGEX="$DATABASE_URL_KEY=\"([^\"]*)\""
-
-if [[ $vars =~ $DB_REGEX ]]; then
-    echo ${BASH_REMATCH[1]}
-else
-    echo "No match"
-fi
-
-
-DB="0b6fe911b091a7c05124.fly.dev:5432\n"
-
-# remove new line using sed
-DB=$(echo $DB | sed 's/\\n//g')
-
-echo $DB
+# create if elseif else statement
+# if NEW_DATABASE_URL is not empty, then run the following
+# if EXISTING_DATABASE_URL is not empty, then run the following
+# else, exit with error
 
