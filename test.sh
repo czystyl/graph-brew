f16102396cef499a8e63b975073c3f59320e2620
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-07 06:35:44 +0100

    Another CI commit

--- test.sh
+++ test.sh
@@ -1,16 +1,11 @@
-ENV_VARS=$(cat .vercel/.env.preview.local)
-DATABASE_URL_KEY=DATABASE_URL_4719c80b6fe911b091a7c05124
-DB_REGEX="$DATABASE_URL_KEY=\"([^\"]*)\""
+PG_CREATE_OUTPUT="Creating postgres cluster in organization product-brew Creating app... Setting secrets on app 4719c80b6fe911b091a7c05124... Provisioning 1 of 1 machines with image flyio/postgres:14 Waiting for machine to start... Machine 6e82e25c703287 is created ==> Monitoring health checks Postgres cluster 4719c80b6fe911b091a7c05124 created Username: postgres Password: FaTwdw7hD1MdLGq Hostname: 4719c80b6fe911b091a7c05124.internal Proxy port: 5432 Postgres port: 5433 Connection string: ***4719c80b6fe911b091a7c05124.internal:5432 Save your credentials in a secure place -- you won't be able to see them again! Connect to postgres Any app within the Product Brew organization can connect to this Postgres using the above connection string Now that you've set up Postgres, here's what you need to understand: https://fly.io/docs/postgres/getting-started/what-you-should-know/"
 
-if [[ $ENV_VARS =~ $DB_REGEX ]]; then
-EXISTING_URL=$(echo ${BASH_REMATCH[1]})
-echo $EXISTING_URL 
-# EXISTING_URL="${EXISTING_URL//$'\n'/''}"
-echo $EXISTING_URL
-EXISTING_URL=$(echo $EXISTING_URL | base64)
-echo $EXISTING_URL
-EXISTING_URL="${EXISTING_URL//$'\n'/''}"
-echo $EXISTING_URL
 
-# echo "URL=$EXISTING_URL" >> $GITHUB_OUTPUT
+# PG_CREATE_OUTPUT="${PG_CREATE_OUTPUT//[$'\t\r\n ']}"
+REGEX="Connection string: (.*) Save"
+
+if [[ $PG_CREATE_OUTPUT =~ $REGEX ]]; then
+INTERNAL_CONNECTION_STRING=${BASH_REMATCH[1]}
 fi
+
+echo $INTERNAL_CONNECTION_STRING
