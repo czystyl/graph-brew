4a09ba5072b863cfc9f717a61c7b1a6343d2a562
czystyl <lukasz.czyszczonik@gmail.com>
2023-02-18 01:21:06 +0100

    Add CI/CD from graph brew project

---
+++ .github/README.md
@@ -0,0 +1,9 @@
+# CI setup
+
+1. Add `DATABASE_URL` to Vercel for Production env
+2. Add to Github secrets
+   - Add `FLY_API_TOKEN`
+   - Add `VERCEL_ORG_ID`
+   - Add `VERCEL_PROJECT_ID`
+   - Add `VERCEL_TOKEN`
+3. Update `fly_postgres_template.toml` for database config
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-
+import BundleAnalyzer from "@next/bundle-analyzer";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
+
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -14,4 +18,4 @@ const config = {
   },
 };
 
-export default config;
+export default withBundleAnalyzer(config);
